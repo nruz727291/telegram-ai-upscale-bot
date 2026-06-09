4d1302776eeec8b136fd53e96681079ed960d7b8
nruz727291 <madarauchiha99962728@gmail.com>
2026-06-09 19:54:49 +0530

    Initialize bot with asyncio and logging setup
    Set up asyncio event loop with uvloop and logging.

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
+import uvloop
+import asyncio
+import logging
 
+uvloop.install()
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s | %(levelname)s | %(name)s | %(message)s"
+)
+
+from bot.client import app
+
+import bot.handlers.start
+import bot.handlers.upscale
+
+
+async def main():
+
+    await app.start()
+
+    me = await app.get_me()
+
+    print(f"✅ Bot Started -> @{me.username}")
+
+    await asyncio.Event().wait()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
