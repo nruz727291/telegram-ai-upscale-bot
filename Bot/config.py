c44ce194330fcf762f5f56b52d6d2595c4fea92e
nruz727291 <madarauchiha99962728@gmail.com>
2026-06-09 19:59:11 +0530

    Load environment variables for bot configuration

--- Bot/config.py
+++ Bot/config.py
@@ -1,1 +1,10 @@
+from os import getenv
+from dotenv import load_dotenv
 
+load_dotenv()
+
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+BOT_TOKEN = getenv("BOT_TOKEN")
+
+REPLICATE_API_TOKEN = getenv("REPLICATE_API_TOKEN")
