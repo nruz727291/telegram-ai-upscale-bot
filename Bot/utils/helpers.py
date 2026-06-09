e76e9433e331d316d112cb58a1129e38370d4002
nruz727291 <madarauchiha99962728@gmail.com>
2026-06-09 19:57:49 +0530

    Add function to generate unique filenames

--- Bot/utils/helpers.py
+++ Bot/utils/helpers.py
@@ -1,1 +1,13 @@
+import os
+import uuid
 
+TEMP_DIR = "temp"
+
+os.makedirs(TEMP_DIR, exist_ok=True)
+
+
+def generate_filename(ext="jpg"):
+    return os.path.join(
+        TEMP_DIR,
+        f"{uuid.uuid4()}.{ext}"
+    )
